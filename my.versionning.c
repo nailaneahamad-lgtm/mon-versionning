630531e6ea6ae7959ca443ae9f2ee24845eab6e1
nailaneahamad-lgtm <nailane.ahamad@gmail.com>
2026-02-25 01:13:39 +0000

    deuxieme question

--- my.versionning.c
+++ my.versionning.c
@@ -2,7 +2,7 @@
 int main(){
     int N,i,j;
     do{
-        prints("Entrer un entier positif: ");
+        printf("Entrer un entier positif: ");
         scanf("%d",&N);
         if(N<=0){
             puts("Erreur: la valeur saisi n'est pas positif.");
@@ -12,6 +12,13 @@ int main(){
     for(i=1;i<=N;i++){
         for(j=1;j<=10;j++){
             printf("%d * %d = %d\n",i,j,i*j);
+        }
+        printf("\n");
+    }
+    printf("\n------------Tableau d'addition----------------\n");
+    for(i=1;i<=N;i++){
+        for(j=1;j<=10;j++){
+            printf("%d + %d = %d\n",i,j,i+j);
         }
         printf("\n");
     }

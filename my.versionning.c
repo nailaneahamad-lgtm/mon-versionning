f635a37b8020fe3806ea576c88d9234601452e5c
nailaneahamad-lgtm <nailane.ahamad@gmail.com>
2026-02-25 00:26:04 +0000

    premiere question

--- my.versionning.c
+++ my.versionning.c
@@ -1,1 +1,19 @@
-
+#include <stdio.h>
+int main(){
+    int N,i,j;
+    do{
+        prints("Entrer un entier positif: ");
+        scanf("%d",&N);
+        if(N<=0){
+            puts("Erreur: la valeur saisi n'est pas positif.");
+        }
+    }while(N<=0);
+    printf("\n------------Tableau de multiplication------------\n");
+    for(i=1;i<=N;i++){
+        for(j=1;j<=10;j++){
+            printf("%d * %d = %d\n",i,j,i*j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
